323694a5a4125c7523da98a7aa4f20ef11c64a7f
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-06-29 16:01:16 -0700

    fixed strong params

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,20 +5,32 @@ class GamesController < ApplicationController
     render json: GameSerializer.new(games)
   end
 
+  def show
+  end
 
   #new/save player-host with game creation
   #and create two teams
 
   def create
-    game = Game.new(game_params)
-    game.save
-    render json: game
+    # byebug
+    game = Game.new(join_code: game_params[1])
+    team_a = Team.new(team_letter: "a", game: game)
+    team_b = Team.new(team_letter: "b", game: game)
+    player1 = Player.new(name: game_params[0], game: game, team: team_a)
+
+    game.host = player1
+
+    game.save!
+    player1.save!
+    team_a.save!
+    team_b.save!
+    render json: GameSerializer.new(game)
   end
 
   private
 
   def game_params
-    params.require(:game).permit(:join_code, :host_id)
+    params.require([:host_name, :join_code])
   end
   
 end
