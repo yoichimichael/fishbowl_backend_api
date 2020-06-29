0ebe32d9c6c05fe440b6737cffd80ad485695ee6
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-06-29 00:04:26 -0700

    created POST to hikes route controller action

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,4 +1,15 @@
 class GameController < ApplicationController
-  def index
+  
+  def create
+    game = Game.new(game_params)
+    game.save
+    render json: game
   end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:join_code, :host_id)
+  end
+  
 end
