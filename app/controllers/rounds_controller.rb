40dca99d0ed76affe07a988a60ae86c594bc4f65
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-08-05 18:26:01 -0700

    working on deleting join table records

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -13,13 +13,19 @@ class RoundsController < ApplicationController
   end
 
   def end_turn
+    submission_ids = round_params[0]
+    submission_ids.each do |id|
+      in_play_card = InPlayCard.find_by(round_id: params[:id], submission_id: id)
+      in_play_card.delete
+    end
+
+    byebug
+
     round = Round.find(params[:id])
     round.clock = 15
     round.turn_part = "end"
     round.save
   end
-
-  
 
   private
 
